4c170e25b1f53d49bb70b3a29868bb07aad384c9
tsingbx <tsingbx@gmail.com>
2023-11-03 12:29:46 +0800

    empty

--- gopls/internal/goxls/astutil/imports.go
+++ gopls/internal/goxls/astutil/imports.go
@@ -292,25 +292,25 @@ func DeleteNamedImport(fset *token.FileSet, f *ast.File, name, path string) (del
 						}
 					}
 			}*/
-			/*
-				if j > 0 {
-					lastImpspec := gen.Specs[j-1].(*ast.ImportSpec)
-					lastLine := fset.PositionFor(lastImpspec.Path.ValuePos, false).Line
-					line := fset.PositionFor(impspec.Path.ValuePos, false).Line
 
-					// We deleted an entry but now there may be
-					// a blank line-sized hole where the import was.
-					if line-lastLine > 1 || !gen.Rparen.IsValid() {
-						// There was a blank line immediately preceding the deleted import,
-						// so there's no need to close the hole. The right parenthesis is
-						// invalid after AddImport to an import statement without parenthesis.
-						// Do nothing.
-					} else if line != fset.File(gen.Rparen).LineCount() {
-						// There was no blank line. Close the hole.
-						fset.File(gen.Rparen).MergeLine(line)
-					}
+			if j > 0 {
+				lastImpspec := gen.Specs[j-1].(*ast.ImportSpec)
+				lastLine := fset.PositionFor(lastImpspec.Path.ValuePos, false).Line
+				line := fset.PositionFor(impspec.Path.ValuePos, false).Line
+
+				// We deleted an entry but now there may be
+				// a blank line-sized hole where the import was.
+				if line-lastLine > 1 || !gen.Rparen.IsValid() {
+					// There was a blank line immediately preceding the deleted import,
+					// so there's no need to close the hole. The right parenthesis is
+					// invalid after AddImport to an import statement without parenthesis.
+					// Do nothing.
+				} else if line != fset.File(gen.Rparen).LineCount() {
+					// There was no blank line. Close the hole.
+					fset.File(gen.Rparen).MergeLine(line)
 				}
-				j--*/
+			}
+			j--
 		}
 	}
 
